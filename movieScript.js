de4857220db29ebcdd28df74c8f96622c607c740
Adrienne Knight <adriennek1@gmail.com>
2020-04-15 11:51:13 -0600

    fixed

--- movieScript.js
+++ movieScript.js
@@ -1,8 +1,19 @@
-$(document).ready(function () {
+document).ready(function () {
 
 
 
 
+
+    //var vodkaBtn = $('#vodka');
+    //everything that happens when you click vodka or each spirt should happen here
+    //including the api call for buttons buttons and appending them to the screen with drinks and appending them to the screewn and 5 movies
+    //$('vodka').on("click", function(){
+    /// run api call that matches this liqour for movie 
+    /// decide what we are going to search by in the api call
+    
+    
+    });
+    
     const BASEURL = "http://www.omdbapi.com/?apikey="
     const APIKEY = "550dba5f"
     const arr = [];
@@ -15,69 +26,68 @@ $(document).ready(function () {
             method: "GET",
             url: URL,
             dataType: "JSON",
-        })
+        }) 
             //executes when GET request is done
             //getting a movie array from the response object called Search
-            ///.done(function (response) {
-            // let movieArr = response.Search;
-            // renderMovies(movieArr);
-            //});
-            //}
-            .then(function (response) {
-
-                //console.log(data);
-                let movieImg = $("<img>").attr("src", response.Search[0].Poster)
-                // picture of movie here
-                $("#movieImg").empty().append(movieImg);
-                // add title of movie
-                $(".titleMovie").text(response.Search[0].Title);
-                // type of movie/show
-                $("#mediaType").text(response.Search[0].Type);
-                // year released
-                $("#year").text(response.Search[0].Year);
-
-            })
-    }
-
-    ///});
-
+            .done(function (response) {
+                let movieArr = response.Search;
+                renderMovies(movieArr);
+                $(".drinkChoice").on("click", function (e) {
+                    e.preventDefault();
+    
+                    
+                        
+                        //console.log(data);
+                        let movieImg = $("<img>").attr("src", response.Search.Poster)
+                        // picture of drink
+                        $("#moviePoster").empty().append(movieImg);
+                        // add title of drink
+                        $(".movieTitle").text(re.drinks[0].strDrink);
+                        // 
+                        $("#drinkInstructions").text("Instructions: " + data.drinks[0].strInstructions);
+                        // add Ingredients
+                        $("#drinkIngredients").text("Ingredients: " + data.drinks[0].strIngredient1 + ", " + data.drinks[0].strIngredient2 + ", " + data.drinks[0].strIngredient3);
+                    });
+    
+                });
+    
+               
+            };
+        
     // make buttons
     $("button1").text(response.Search.Title[0]);
-    $("button2").text(response.Search.Title[0]);
-    $("button3").text(response.Search.Title[0]);
-    $("button4").text(response.Search.Title[0]);
-    $("button5").text(response.Search.Title[0]);
-
+    
     //card
     //function to match each button with response, goes on card
-    $("button1").on("click", function () {
-        //$("#title1").text(response.Search.Title[0]);
-        //title that matches with button 1 to go in the card 
-        //$("#year1").text(response.Search.Year[0])  
-        //}
-
-
-
-
-
-        // API page limit 1-100
-        function getNumMoviePages(limit) {
-            for (let index = 1; index < limit; index++) {
-                startMovieRequest("speed", index)
-            }
+    //$("button1").on("click", function(){
+    //$("#title1").text(response.Search.Title[0]);
+    //title that matches with button 1 to go in the card 
+    //$("#year1").text(response.Search.Year[0])  
+    //}
+    
+    
+    
+      
+         
+    // API page limit 1-100
+    function getNumMoviePages(limit) {
+        for (let index = 1; index < limit; index++) {
+          startMovieRequest("speed",index)
         }
-
-        //random index of the full movie array
-        function getRandomArrayIndex(arr) {
-            return Math.floor(Math.random() * arr.length)
+    }
+    
+    //random index of the full movie array
+    function getRandomArrayIndex(arr) {
+        return Math.floor(Math.random() * arr.length)
+    }
+    //Currently only passed in 10 length array
+    // need larger array of all movies from pages combined into one array
+    function renderMovies(arr) {
+        var selectedMovies = []
+        for (var i = 0; i < 5; i++) {
+            var random = getRandomArrayIndex(arr)
+            selectedMovies.push(arr[random])
         }
-        //Currently only passed in 10 length array
-        // need larger array of all movies from pages combined into one array
-        function renderMovies(arr) {
-            var selectedMovies = []
-            for (var i = 0; i < 5; i++) {
-                var random = getRandomArrayIndex(arr)
-                selectedMovies.push(arr[random])
-            }
-
-    };
+    
+    }
+    
